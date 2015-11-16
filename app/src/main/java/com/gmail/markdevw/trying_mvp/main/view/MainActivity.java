05a1a808899571cdf0863c880963cc0e4ac25023
Mark Wassef <markdevw@gmail.com>
2015-11-16 17:26:03 -0500

    Added SearchView to Toolbar which will call a presenter method to perform a search

--- app/src/main/java/com/gmail/markdevw/trying_mvp/main/view/MainActivity.java
+++ app/src/main/java/com/gmail/markdevw/trying_mvp/main/view/MainActivity.java
@@ -1,10 +1,13 @@
 package com.gmail.markdevw.trying_mvp.main.view;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.gmail.markdevw.trying_mvp.R;
 import com.gmail.markdevw.trying_mvp.main.presenter.ItemPresenter;
@@ -37,6 +40,21 @@ public class MainActivity extends AppCompatActivity implements MainView {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+            itemPresenter.searchForItems(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
     }
 }
