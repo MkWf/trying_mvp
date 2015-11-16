8fc5759f8c52c65cb1e0ceebb8517ea2a716e56f
Mark Wassef <markdevw@gmail.com>
2015-11-16 18:18:19 -0500

    Completed ItemAdapter

--- app/src/main/java/com/gmail/markdevw/trying_mvp/main/adapters/ItemAdapter.java
+++ app/src/main/java/com/gmail/markdevw/trying_mvp/main/adapters/ItemAdapter.java
@@ -4,11 +4,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.gmail.markdevw.trying_mvp.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Mark on 11/16/2015.
@@ -28,7 +32,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
 
     @Override
     public void onBindViewHolder(ItemAdapterViewHolder itemAdapterViewHolder, int index) {
-
+        itemAdapterViewHolder.update(items.get(index));
     }
 
     @Override
@@ -37,9 +41,17 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
     }
 
     class ItemAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.item_title)
+        TextView title;
         
         public ItemAdapterViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void update(String item) {
+            title.setText(item);
         }
     }
 }
