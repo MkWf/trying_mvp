b5a7c758cb64791cb12ca111ffd516f9c3056c02
Mark Wassef <markdevw@gmail.com>
2015-11-16 16:41:04 -0500

    Injected RecylerView and Toolbar into MainActivity. Set Toolbar

--- app/src/main/java/com/gmail/markdevw/trying_mvp/view/MainActivity.java
+++ app/src/main/java/com/gmail/markdevw/trying_mvp/view/MainActivity.java
@@ -1,15 +1,29 @@
 package com.gmail.markdevw.trying_mvp.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.gmail.markdevw.trying_mvp.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.rv_activity_main)
+    RecyclerView mRecyclerView;
+
+    @Bind(R.id.tb_activity_main)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
     }
 }
