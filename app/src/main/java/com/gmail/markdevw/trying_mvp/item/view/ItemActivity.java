e653f72f161421b8bf9ad8e8c3038700129c104e
Mark Wassef <markdevw@gmail.com>
2015-11-16 19:38:16 -0500

    Filled out ItemActivity and activity_item to receive the item data and display it

--- app/src/main/java/com/gmail/markdevw/trying_mvp/item/view/ItemActivity.java
+++ app/src/main/java/com/gmail/markdevw/trying_mvp/item/view/ItemActivity.java
@@ -4,20 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.gmail.markdevw.trying_mvp.R;
+
+import butterknife.Bind;
 
 /**
  * Created by Mark on 11/16/2015.
  */
 public class ItemActivity extends AppCompatActivity {
 
+    private static final String ITEM_TITLE = "item_title";
+
     public static Intent newIntent(Context context, String title) {
         Intent intent = new Intent(context, ItemActivity.class);
-        intent.putExtra("item_title", title);
+        intent.putExtra(ITEM_TITLE, title);
         return intent;
     }
+
+    @Bind(R.id.activity_item_title)
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        title.setText(getIntent().getExtras().getString(ITEM_TITLE));
     }
 }
