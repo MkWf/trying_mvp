590d055387f6d2d76ef7ee5b1df200a6598d66f8
Mark Wassef <markdevw@gmail.com>
2015-11-16 19:22:20 -0500

    Added ItemActivity

---
+++ app/src/main/java/com/gmail/markdevw/trying_mvp/item/view/ItemActivity.java
@@ -0,0 +1,16 @@
+package com.gmail.markdevw.trying_mvp.item.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mark on 11/16/2015.
+ */
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
