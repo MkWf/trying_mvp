45981884d26f068f2b8412cd919b994a6e4b5a53
Mark Wassef <markdevw@gmail.com>
2015-11-18 14:32:38 -0500

    Revert "Presenter now holds the View as a WeakReference to prevent memory leaks. Updated Presenter methods"
    This reverts commit ee4eef57759cc16cf947a43702f11ac7ce41e0be.

--- app/src/main/java/com/gmail/markdevw/trying_mvp/main/presenter/MainPresenter.java
+++ app/src/main/java/com/gmail/markdevw/trying_mvp/main/presenter/MainPresenter.java
@@ -4,7 +4,6 @@ import com.gmail.markdevw.trying_mvp.main.interactors.ItemInteractor;
 import com.gmail.markdevw.trying_mvp.main.interactors.MainInteractor;
 import com.gmail.markdevw.trying_mvp.main.view.MainView;
 
-import java.lang.ref.WeakReference;
 import java.util.List;
 
 /**
@@ -12,38 +11,29 @@ import java.util.List;
  */
 public class MainPresenter implements ItemPresenter, OnSearchFinishedListener{
 
-    private WeakReference<MainView> mainView;
+    private MainView mainView;
     private ItemInteractor itemInteractor;
 
-    public MainPresenter(WeakReference<MainView> mainView) {
+    public MainPresenter(MainView mainView) {
         this.mainView = mainView;
         itemInteractor = new MainInteractor();
     }
 
     @Override
     public void performSearch(String search) {
-        if(mainView != null){
-            MainView view = mainView.get();
-            view.showProgressBar();
-            itemInteractor.searchForItems(search, this);
-        }
+        mainView.showProgressBar();
+        itemInteractor.searchForItems(search, this);
     }
 
     @Override
     public void onSuccess(List<String> result) {
-        if(mainView != null){
-            MainView view = mainView.get();
-            view.hideProgressBar();
-            view.setAdapter(result);
-        }
+        mainView.hideProgressBar();
+        mainView.setAdapter(result);
     }
 
     @Override
     public void onError(String search) {
-        if (mainView != null) {
-            MainView view = mainView.get();
-            view.hideProgressBar();
-            view.showError(search);
-        }
+        mainView.hideProgressBar();
+        mainView.showError(search);
     }
 }
